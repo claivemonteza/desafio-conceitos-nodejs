d5d32c19e62cae875f740371af0e96dfe87f208e
Claive Monteza <43734209+claivemonteza@users.noreply.github.com>
2023-11-27 15:08:57 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,31 +2,54 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.json());
+let repository = { id: 1, title: 'Desafio Node.js', url: 'http://github.com/...', techs: ["Node.js", "..."], likes: 0 }
 
+const repositores = [repository];
 
 app.get('/repositories', (req, res) => {
-    res.json({ mensagem: `Você forneceu o parâmetro: ${parametro}` });
+    res.json(repositores);
 });
 
-app.post('/:repository', (req, res) => {
+app.post('/repositories', (req, res) => {
   const dados = req.body;
-  res.json({ mensagem: 'Dados recebidos com sucesso', dados });
+  repositores.push(dados);
+  res.json({ mensagem: 'Repositorio registrado com sucesso', dados });
 });
 
 
-app.put('/:repository/:id', (req, res) => {
+app.put('/repositories:id', (req, res) => {
   const dados = req.body;
-  res.json({ mensagem: 'Dados recebidos com sucesso', dados });
+  const repository = repositores.find(x=>x.id==req.params.id);
+  if(repository === undefined){
+    res.json({ mensagem: 'Repositorio não encontrado', dados });
+  }
+  repository.title = dados.title;
+  repository.techs = dados.techs;
+  repository.url = dados.url;
+  repository.likes=dados.likes;
+
+  res.json({ mensagem: 'Repositorio actulizado com sucesso', repository });
 });
 
-app.delete('/:repository/:id', (req, res) => {
+app.delete('/repositories/:id', (req, res) => {
   const dados = req.body;
-  res.json({ mensagem: 'Dados recebidos com sucesso', dados });
+  const indexOf = repositores.findIndex(x=>x.id==req.params.id);
+  if(indexOf === -1){
+    res.json({ mensagem: 'Repositorio não encontrado', dados });
+  }
+  repositores.splice(indexOf,1);
+  res.json({ mensagem: 'Repositorio removido com sucesso', dados });
 });
 
-app.post('/:repository/:id/like', (req, res) => {
+app.post('/repositories/:id/like', (req, res) => {
   const dados = req.body;
-  res.json({ mensagem: 'Dados recebidos com sucesso', dados });
+  const repository = repositores.find(x=>x.id==req.params.id);
+  if(repository === undefined){
+    res.json({ mensagem: 'Repositorio não encontrado', dados });
+  }
+
+  repository.likes++;
+  res.json({ mensagem: 'Foi feito like no repositorio', repository });
 });
 
 // Inicia o servidor na porta especificada
